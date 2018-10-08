778b518bb1f965c5b8ccfe9ff868eaf5b77837a8
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-10-09 02:30:28 +0530

    deployed and tested

--- Assembly.sol
+++ Assembly.sol
@@ -20,4 +20,35 @@ _r := add(1,_r)
 jumpi(loop, lt(i,10))
 }
 }
+function nativeCondition(uint v) public returns(uint r)    //high level
+{
+if(v=5)
+{
+return 55;
 }
+if(v=6)
+{
+return 66;
+}
+return 11;
+}
+function asmCond(uint v) public returns(uint _r)  //assembly
+{
+assembly
+{
+switch _v
+case 5
+{
+_r:= 55
+}
+case 5
+{
+_r:= 66
+}
+default 
+{
+_r:= 11
+}
+}
+}
+// assembly language consumes less gas when compared to high level language
