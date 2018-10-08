224b58bc93963b5d1d69023ac0b913ec4b3f0f18
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-10-09 02:56:58 +0530

    Update interactionContracts.sol

--- interactionContracts.sol
+++ interactionContracts.sol
@@ -5,8 +5,6 @@ contract EtherTransferTo
 {
 function () public payable 
 {
-
-
 }
 function getBalance () public returns(uint)
 {
@@ -15,4 +13,17 @@ return address(this).balance;
 
 contract EtherTransferFrom   //this contract transfers ether to contract EtherTransferTo 
 {
-
+EtherTransferTo private _instance;
+function EtherTransferFrom () public
+{
+_instance = new EtherTransferTo();
+}
+function getBalance () public returns(uint)
+{
+return address(this).balance;
+}
+function getBalanceOfInstance() public returns (uint)
+{
+return address(_instance).balance;
+}
+}
